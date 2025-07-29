39dc08e02185df1842a448329fb4facb5085498e
Nitin Rana <91018821+zedted0112@users.noreply.github.com>
2025-07-29 01:58:09 -0700

    GeethaBeautyParlor

---
+++ src/components/About/AboutInfo.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import infoBG from "/footerBG.png";
+import infoPeople from "/bg-bride3.png";
+const AboutInfo = () => {
+  return (
+    <div
+      className="w-full bg-[#937D64AD]"
+     
+    >
+      <div className="md:pt-32 py-12 flex md:flex-row flex-col md:p-0 p-10">
+        <div
+          className="md:w-[50%] w-full md:h-[750px] h-[500px] brightness-50  rounded-tr-[54px]"
+          style={{
+            backgroundImage: `url('${infoPeople}')`,
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "cover",
+            objectFit: "cover",
+            backgroundPosition:"center"
+          }}
+        >
+        </div>
+
+        <div className="md:w-[50%] h-full mx-auto max-w-[1200px] flex flex-col gap-12 md:pl-12 lg:pr-32 md:pr-16 pb-24 md:px-10">
+          <h1 className="md:text-[50px] md:mt-0 mt-5 text-[40px] text-black leading-[57.6px] tracking-[-1.26px]">
+          Redefining Beauty in Uttarkashi.
+          </h1>
+          <p className="text-[20px] text-black leading-[28px] tracking-[-0.5px]">
+          Welcome to Geeta Makeovers — where beauty meets serenity in the heart of Uttarkashi.
+With over 15 years of experience, we craft timeless looks for every occasion.
+From radiant bridal makeovers to everyday elegance, we transform your vision into reality — one brushstroke at a time.                </p>
+          <p className="text-[20px] text-black leading-[28px] tracking-[-0.5px]">
+          At Geeta Makeovers, beauty isn’t just applied — it’s revealed.
+          In the peaceful folds of Uttarkashi, we shape moments into memories and looks into lasting confidence.          </p>
+          <button className="bg-[#251408] w-[200px] transition-all duration-700 ease-in-out  hover:scale-95  text-[17px] leading-[19.58px] tracking-[-0.43px] text-white px-8 py-6 rounded-tr-xl">
+            More
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AboutInfo;
---
+++ src/components/Contacts/ContactHero.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import heroImageContact from "/bg-bride6.jpg";
+import Header from "../Header";
+
+const ContactHero = () => {
+  return (
+    <div className="w-full min-h-screen bg-cover bg-center overflow-hidden" style={{ 
+      backgroundImage: `url('${heroImageContact}')`,
+      backgroundPosition: 'top center'
+    }}>
+      <div className="w-full h-full bg-[#32210DB2]">
+        <Header transparent={false} />
+        <div className="w-[80%] mx-auto max-w-[1200px] md:pt-36 md:pb-56 pt-20 pb-32 flex flex-col gap-6 justify-center items-center">
+          <h1 className="md:text-[60px] text-[45px] lg:px-60 text-center text-white md:leading-[80px] tracking-[-2.01px]">
+            Begin Your Beauty Transformation with Glamour Palette
+          </h1>
+          <p className="text-white text-[20px] leading-[35px] lg:w-[50%] md:w-[70%] tracking-[-0.5px] text-center">
+            Ready to elevate your beauty experience? Our team at Glamour Palette is here to accompany you through every step of your beauty journey. Reach out now and let's bring your beauty dreams to life.
+          </p>
+          <button className="border transition-all duration-700 ease-in-out hover:scale-95 border-[#937D64]">
+            <h1 className="text-white text-[17px] leading-[19.58px] px-14 py-3">
+              EXPLORE MORE
+            </h1>
+          </button>
+        </div>
+      </div>
+    </div>
+    
+  );
+};
+
+export default ContactHero;
